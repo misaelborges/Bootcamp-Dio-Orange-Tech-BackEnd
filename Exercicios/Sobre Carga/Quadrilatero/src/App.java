718cd33bf63bf39c36b3a908def02e701579d9ff
Misael Borges <mizaelborges2011@gmail.com>
2023-01-03 11:48:40 -0300

    Exercicios de Sobre Carga basico

---
+++ Exercicios/Sobre Carga/Quadrilatero/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 9);
+    }
+}
