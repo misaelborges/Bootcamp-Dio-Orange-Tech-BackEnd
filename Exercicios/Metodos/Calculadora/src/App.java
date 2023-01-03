538139c46401b08d769ba1a3eb172bba82925960
Misael Borges <mizaelborges2011@gmail.com>
2023-01-03 11:28:09 -0300

    exercicios basicos de metodos

---
+++ Exercicios/Metodos/Calculadora/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+    }
+}
---
+++ Exercicios/Metodos/Emprestimo/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
