89de171b5a00e31cf7aeee7cea95a38e7fd8d188
Misael Borges <mizaelborges2011@gmail.com>
2023-01-19 16:59:27 -0300

    criando uma REST API

---
+++ my-first-web-api/src/main/java/dio/webapi/MyFirstWebApiApplication.java
@@ -0,0 +1,12 @@
+package dio.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstWebApiApplication.class, args);
+	}
+}
--- primeiros-passos/src/main/java/dio/springboot/Calculadora.java
+++ primeiros-passos/src/main/java/dio/springboot/Calculadora.java
@@ -7,4 +7,4 @@ public class Calculadora {
     public int somar(int numero1, int numero2) {
         return numero1+numero1;
     }
-}
+}                    
